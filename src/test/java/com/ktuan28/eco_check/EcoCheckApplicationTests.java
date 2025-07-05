69f3fa395107dd9f2ab310f593ad326c1c771b96
ktuanvxhg28 <kdtuan28@gmail.com>
2025-07-05 21:30:00 +0700

    first commit

---
+++ src/test/java/com/ktuan28/eco_check/EcoCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktuan28.eco_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
